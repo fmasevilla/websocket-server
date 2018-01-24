d6eb47927fd2da4d44f227ee4b0ef2dd0f3bc7ba
Francis Sevilla <francis@francissevilla.com>
2018-01-24 12:57:39 +0800

    fixed node chat server

--- index.js
+++ index.js
@@ -2,28 +2,35 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var redis = require('redis').createClient({redis_url:'redis://127.0.0.1:6379'});
+var got = require('got');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+//   socket.on('chat', function(msg){
+//   	console.log('message: ' + data["message"]);
+//     console.log('auth_token: ' + data["auth_token"]);
+//     console.log('chat_channel_id: ' + data["chat_channel_id"]);
+//   	sendMessage(msg);
+//   });
 });
 
 redis.on('message', function(channel, message){
 	var info = JSON.parse(message);
-	io.sockets.emit(channel, info);
+	var new_channel = (channel + "/" + info["chat_channel_id"]);
+	console.log("info: " + info["chat_channel_id"]);
+	console.log("channel: " + channel);
+	io.sockets.emit(new_channel, info);
 });
 
 redis.subscribe('chat');
 
-http.listen(8000, function(){
-  console.log('listening on *:8000');
+http.listen(8081, function(){
+  console.log('listening on *:8081');
 });
